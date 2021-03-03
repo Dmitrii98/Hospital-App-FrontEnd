2671ad60ecd0bb2f629f0af007ba271b6c7f035d
user <dimasurdin@yandex.ru>
2021-03-03 15:59:28 +0300

    Add logic for register page

--- src/Components/EntryField/SignUp/SignUp.js
+++ src/Components/EntryField/SignUp/SignUp.js
@@ -1,11 +1,48 @@
-import React from 'react';
+import React, {useState} from 'react';
+import axios from "axios";
 import {
   TextField,
-  Button
+  Button,
+  Snackbar
 } from '@material-ui/core';
+import MuiAlert from '@material-ui/lab/Alert';
 import SignUpStyles from './SignUpStyles.css';
 
+function Alert(props) {
+  return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
+
 function SignUp() {
+  const [user, setUser] = useState([]);
+  const [login, setLogin] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordRepeat, setPasswordRepeat] = useState('');
+  const [open, setOpen] = React.useState(false);
+
+  const handleClick = () => {
+    setOpen(true);
+  }
+
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setOpen(false);
+  }
+
+  const registerUser = async () => {
+    await axios.post('http://localhost:8000/createUser', {
+      login,
+      password
+    }).then(res => {
+      setLogin('');
+      setPassword('');
+      setPasswordRepeat('');
+      setUser(res.data.data);
+    })
+  }
+
   return (
     <div className="sign-up">
       <h2 className='header-text'>
@@ -15,10 +52,13 @@ function SignUp() {
         <div className='login'>
           <p>Login:</p>
           <TextField
-            id="outlined-basic"
+            type='text'
+            id='outlined-basic'
             className='login-input'
-            variant="outlined"
+            variant='outlined'
             placeholder='Login'
+            value={login}
+            onChange={(e) => setLogin(e.target.value)}
           />
         </div>
         <div className='password'>
@@ -30,6 +70,8 @@ function SignUp() {
             className='password-input'
             variant="outlined"
             placeholder='Password'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
           />
         </div>
         <div className='password'>
@@ -41,16 +83,31 @@ function SignUp() {
             className='password-input'
             variant="outlined"
             placeholder='Repeat password'
+            value={passwordRepeat}
+            onChange={(e) => setPasswordRepeat(e.target.value)}
           />
         </div>
         <div className='buttons'>
           <Button
             className='register-btn'
             variant="outlined"
+            onClick={() => {
+              if (password === passwordRepeat) {
+                registerUser();
+              } else {
+                setPassword('');
+                setPasswordRepeat('');
+                setOpen(true);
+                }
+              }
+            }
           >
             Зарегистрироваться
           </Button>
           <Button className='authorization-btn'>Авторизоваться</Button>
+          <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+            <Alert severity="error">Пароли не совпадают</Alert>
+          </Snackbar>
         </div>
       </div>
     </div>
